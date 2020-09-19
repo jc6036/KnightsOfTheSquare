87e250320a6f47b92980c5a4975abb8e4655c4b6
Jesse Cabell <jc6036@gmail.com>
2020-09-19 10:11:54 -0400

    Initial Updates - Bot API added

--- KnightsOfTheSquare.py
+++ KnightsOfTheSquare.py
@@ -2,3 +2,22 @@
 # Author: Jesse Cabell
 # Licensed under GPL 2.0
 # 09/19/2020
+
+import discord
+
+# Quick prototype code
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+client.run('NzU2ODcwNzUwODc4ODI2NTM1.X2YI0A.CvBFkLTD_dKTJ7pIrUazUg9Lu2w')
--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # KnightsOfTheSquare
 A chess bot for discord. Play games with other members of your server using chess notation.
 
+Use this link to add the bot to your server: (Note that public access is not available yet)
+https://discord.com/api/oauth2/authorize?client_id=756870750878826535&permissions=109632&scope=bot
+
 README TODO: List all available commands here, and a basic description of how to do chess notation.
